3781c28170b950022c8f8b6fe0486eeda8e5c81b
fsinfrastructure <fsnt@five-startech.com>
2025-03-03 12:48:14 -0500

    Update spamhaus.sh

--- spamhaus.sh
+++ spamhaus.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 rm drop*
-spamhaus_drop_list.txt
+#rm spamhaus_drop_list.txt
 wget https://www.spamhaus.org/drop/drop_v4.json
 jq -r 'select(.type == null) | (.cidr)' drop_v4.json > spamhaus_drop_list.txt
